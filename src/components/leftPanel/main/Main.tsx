5743012df197afb05a62e141a63ba484c507ea83
Jessica Cardeño <cardenojessica07@gmail.com>
2024-01-10 22:37:59 -0500

    Input validations

--- src/components/leftPanel/main/Main.tsx
+++ src/components/leftPanel/main/Main.tsx
@@ -1,7 +1,19 @@
 import styles from "./Main.module.css";
 import iconArrow from "../../../assets/images/icon-arrow.svg";
+import { useState } from "react";
 
 function Main() {
+  const [email, setEmail] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  const handleClick = () => {
+    if (email === "" || !emailRegex.test(email)) {
+      setErrorMsg("Please provide a valid input");
+    }
+  };
+
   return (
     <div className={styles.mainContainer}>
       <h1>we're</h1>
@@ -14,8 +26,19 @@ function Main() {
         and our launch deals.
       </p>
       <div className={styles.formContainer}>
-        <input type='text' placeholder='Email Adress' />
-        <div className={styles.imgContainer}>
+        <input
+          type='text'
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            if (errorMsg !== "") {
+              setErrorMsg("");
+            }
+          }}
+          placeholder='Email Adress'
+        />
+        <span className={styles.errorMsg}>{errorMsg}</span>
+        <div onClick={handleClick} className={styles.imgContainer}>
           <img src={iconArrow} alt='' />
         </div>
       </div>
