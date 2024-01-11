24d62044c411b88a7ba1783806c0c0f7d5648501
Jessica Cardeño <cardenojessica07@gmail.com>
2024-01-10 21:46:24 -0500

    Fixing structure and mobile desing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/header/Header";
 import LeftPanel from "./components/leftPanel/LeftPanel";
 import RightPanel from "./components/rightPanel/RightPanel";
 
 function App() {
   return (
     <div className='Wrapper'>
+      <Header />
       <LeftPanel />
       <RightPanel />
     </div>
--- src/components/leftPanel/LeftPanel.tsx
+++ src/components/leftPanel/LeftPanel.tsx
@@ -1,11 +1,9 @@
 import styles from "./LeftPanel.module.css";
-import Header from "./header/Header";
 import Main from "./main/Main";
 
 function LeftPanel() {
   return (
     <div className={styles.leftPanelContainer}>
-      <Header />
       <Main />
     </div>
   );
